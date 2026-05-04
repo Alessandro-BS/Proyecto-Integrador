6345bd2368a242b58e9a4fc5a45bf31f5ed17eec
Alessandro-BS <fabrizio.bs9012@gmail.com>
2026-05-04 02:50:48 -0500

    Creación Entity: Especialidad

---
+++ src/main/java/com/sisol/salud/model/entity/Especialidad.java
@@ -0,0 +1,44 @@
+package com.sisol.salud.model.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "especialidades")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Especialidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombre;
+
+    @Column(nullable = true, length = 500)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private boolean activo = true; // Activo por defecto
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
