b4401c629fd4929f0ea7a43757eddb25a6b5049b
Alessandro-BS <fabrizio.bs9012@gmail.com>
2026-05-04 01:33:16 -0500

    Spring Initializr + pom.xml + application.yml

---
+++ src/main/java/com/sisol/salud/SisolSaludApplication.java
@@ -0,0 +1,13 @@
+package com.sisol.salud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisolSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisolSaludApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisol/salud/SisolSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisol.salud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisolSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
